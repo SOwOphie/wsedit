4482c7edc7f619aa2c2762278e8a6b244beaed68
Felix Hirn <felix.hirn@wyvernscale.com>
2016-06-24 10:15:18 +0200

    Fixed wrong filename in lang/install.sh

--- lang/install.sh
+++ lang/install.sh
@@ -7,7 +7,7 @@ if [ "$1" ]; then
 	echo >>"$fl"
 	echo "# $1: added by lang/install.sh on `date`:" >>"$fl"
 	echo >>"$fl"
-	cat "lang/$1.wsconf" >> "$HOME/.config/wsedit.conf"
+	cat "lang/$1.wsconf" >> "$HOME/.config/wsedit.wsconf"
 else
 	echo "Please specify one of the following available languages:"
 	ls lang | grep "wsconf" | sed 's:\(.*\)\.wsconf:\1:'
